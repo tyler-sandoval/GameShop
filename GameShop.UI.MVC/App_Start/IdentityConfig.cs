3c5a1096146b2357fdb373f09f7916997807a53c
TYLERSANDOVAL\Student <tyler.sandoval@outlook.com>
2018-08-09 00:05:50 -0500

    COMMIT: Fully converted template & share _layout; updated links to Html.ActionLink;

--- GameShop.UI.MVC/App_Start/IdentityConfig.cs
+++ GameShop.UI.MVC/App_Start/IdentityConfig.cs
@@ -114,8 +114,8 @@ namespace GameShop.UI.MVC.Models
         public static void InitializeIdentityForEF(ApplicationDbContext db) {
             var userManager = HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
             var roleManager = HttpContext.Current.GetOwinContext().Get<ApplicationRoleManager>();
-            const string name = "admin@example.com";
-            const string password = "Admin@123456";
+            const string name = "tyler.sandoval@outlook.com";
+            const string password = "P@ssw0rd";
             const string roleName = "Admin";
 
             //Create Role Admin if it does not exist
