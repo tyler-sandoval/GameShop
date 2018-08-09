14105e79d2adaf58e8e5b2f5c88688a17329e46b
TYLERSANDOVAL\Student <tyler.sandoval@outlook.com>
2018-08-08 21:52:13 -0500

    COMMIT: Added scaffolding for controllers and views; added links under admin auth to indexes; added DBBackup scripts to App_Data;

---
+++ GameShop.UI.MVC/Controllers/ProductCategoriesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GameShop.DATA.EF;
+
+namespace GameShop.UI.MVC.Controllers
+{
+    public class ProductCategoriesController : Controller
+    {
+        private GameShopEntities db = new GameShopEntities();
+
+        // GET: ProductCategories
+        public ActionResult Index()
+        {
+            var productCategories = db.ProductCategories.Include(p => p.Category).Include(p => p.Product);
+            return View(productCategories.ToList());
+        }
+
+        // GET: ProductCategories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductCategory productCategory = db.ProductCategories.Find(id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductName");
+            return View();
+        }
+
+        // POST: ProductCategories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductCategoryID,CategoryID,ProductID")] ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProductCategories.Add(productCategory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", productCategory.CategoryID);
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductName", productCategory.ProductID);
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductCategory productCategory = db.ProductCategories.Find(id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", productCategory.CategoryID);
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductName", productCategory.ProductID);
+            return View(productCategory);
+        }
+
+        // POST: ProductCategories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductCategoryID,CategoryID,ProductID")] ProductCategory productCategory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(productCategory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", productCategory.CategoryID);
+            ViewBag.ProductID = new SelectList(db.Products, "ProductID", "ProductName", productCategory.ProductID);
+            return View(productCategory);
+        }
+
+        // GET: ProductCategories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProductCategory productCategory = db.ProductCategories.Find(id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(productCategory);
+        }
+
+        // POST: ProductCategories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProductCategory productCategory = db.ProductCategories.Find(id);
+            db.ProductCategories.Remove(productCategory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
